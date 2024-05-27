f34dc840c414b6bf6dd970c3acacbb3a9485082a
zainab khimji_zk <165508985+zainabkhimji2@users.noreply.github.com>
2024-05-27 12:48:36 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+const randomNumber = Math.floor(Math.random() *6 + 1)
+
+
+let answer = await inquirer.prompt([{
+    name:"guessNumber",
+    type:"number",
+    message:"Please guess a number between 1 till 6"
+}])
+if(answer.guessNumber === randomNumber){
+    console.log("Congratulation! You Guessed A Right Number");
+    
+}else{
+    console.log("You Loss ! try again");
+    
+}
